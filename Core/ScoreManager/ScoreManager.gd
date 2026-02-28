605ec2589a5fa683da59351a32ee27ea9d301f5d
Anton Kuzan <antonkuzan@gmail.com>
2026-02-28 20:31:02 +0300

    Added simple score manager

---
+++ Core/ScoreManager/ScoreManager.gd
@@ -0,0 +1,30 @@
+extends Node2D
+class_name ScoreManager
+
+@onready var score_label: Label = $ScoreLabel
+
+var total_score: int
+var score_tween: Tween
+
+
+func _ready() -> void:
+	SignalBus.on_flower_collide.connect(on_flower_collide)
+
+
+func on_flower_collide(position: Vector2, data: FlowerData, flower_a: RigidBody2D, flower_b: RigidBody2D) -> void:
+	total_score += 1
+	score_label.text = "%03d" % total_score
+	score_animation()
+
+
+func score_animation() -> void:
+	if score_tween:
+		score_tween.kill()
+
+	$ScoreLabel.scale = Vector2.ONE
+
+	score_tween = get_tree().create_tween()
+	score_tween.set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+
+	score_tween.tween_property($ScoreLabel, "scale", Vector2(1.25, 1.25), 0.2)
+	score_tween.tween_property($ScoreLabel, "scale", Vector2.ONE, 0.2)
